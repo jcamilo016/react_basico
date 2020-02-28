f9469971656334db14e0c0aa7c0f8efdf9d706fe
Juan Camilo <jcamilo016@gmail.com>
2020-02-28 11:37:55 -0500

    Query params

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import queryString from 'query-string';
 
 const Product = (props) => {
+    const queryParams = queryString.parse(props.location.search);
+
     return (
-        <h1>Product {props.match.params.product}</h1>
+        <React.Fragment>
+            <h1>Product {props.match.params.product}</h1>
+            <p>Query Params</p>
+            <ul>
+                {props.location.search && (
+                    Object.entries(queryParams).map((query) => {
+                        return (<li>{`${query[0]}: ${query[1]}`}</li>);
+                    }))
+                }
+            </ul>
+        </React.Fragment>
     )
 };
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -5,10 +5,10 @@ const Products = (props) => {
     return (
         <ul>
             <li>
-                <Link to="/products/A">Producto A</Link>
+                <Link to="/products/A?cupon=react&descuento=10&empresa=technisys">Producto A</Link>
             </li>
             <li>
-                <Link to="/products/B">Producto B</Link>
+                <Link to="/products/B?cupon=react&descuento=20&empresa=10Pearls">Producto B</Link>
             </li>
         </ul>
     );
